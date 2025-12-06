5f7ffeca2145581f69fbe7be56d51fc91ba01b3f
LvnLx <Levin.Leesemann@gmail.com>
2025-12-06 16:50:56 -0600

    Auto delete old images

--- terraform/modules/foundation/artifact_registry.tf
+++ terraform/modules/foundation/artifact_registry.tf
@@ -1,4 +1,24 @@
 resource "google_artifact_registry_repository" "docker" {
   repository_id      = var.project_name
   format             = "DOCKER"
+
+  cleanup_policies {
+    id     = "keep-latest"
+    action = "KEEP"
+
+    most_recent_versions {
+      package_name = "*"
+      keep_count   = 1
+    }
+  }
+
+  cleanup_policies {
+    id     = "delete-others"
+    action = "DELETE"
+
+    condition {
+      package_name = "*"
+      tag_state    = "ANY"
+    }
+  }
 }
