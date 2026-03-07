3900f1821499a6882101ceb9ee89a4fedf05e602
Levin Leesemann <39525477+LevinLeesemann@users.noreply.github.com>
2026-03-07 11:50:20 -0600

    Update exclusion list

--- src/main/java/com/lvnlx/job/posting/notifier/client/AppleClient.java
+++ src/main/java/com/lvnlx/job/posting/notifier/client/AppleClient.java
@@ -21,7 +21,7 @@ public class AppleClient extends Client<AppleJob> {
                 bigQuery,
                 httpService,
                 notificationService,
-                List.of("manager", "research", "testing", "hardware", "site reliability"),
+                List.of("manager", "research", "testing", "hardware", "site reliability", "ml"),
                 List.of("engineer")
         );
     }
